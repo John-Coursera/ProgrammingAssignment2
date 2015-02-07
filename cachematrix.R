8f1fc2f013ac5387a7b61ca4f481d1b269c4bfec
John-Coursera <marriott+coursera@gmail.com>
2015-02-07 16:27:06 -0600

    Implement cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Implements a cacheMatrix data structure that holds a square, invertible
+## matrix with a cached inverse
 
-## Write a short comment describing this function
+## creates a cacheMatrix from a square, invertible matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Gives the inverse of a cacheMatrix.  
+## Returns the cached inverse if it has already been computed, 
+## otherwise will compute, cache, and return the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
